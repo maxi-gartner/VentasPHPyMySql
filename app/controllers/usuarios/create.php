9e100561290893f290a81129a2da35294fdd068f
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-03-06 15:20:55 -0300

    :lock: encriptando contraseñas

--- app/controllers/usuarios/create.php
+++ app/controllers/usuarios/create.php
@@ -7,6 +7,12 @@ $apellido = $_POST['apellido'];
 $email = $_POST['email'];
 $password_user = $_POST['password_user'];
 $password_repeat = $_POST['password_repeat'];
+if($password_user === $password_repeat){
+    $opciones = ['cost' => 10];
+    $password_encriptada = password_hash($password_user, PASSWORD_BCRYPT, $opciones );
+}else{
+    echo "Las contraseñas no coinciden";
+}
 
 $sentencia = $pdo->prepare("INSERT INTO tb_usuarios 
     (nombres, apellido, email, password_user, fyh_creacion) 
@@ -15,6 +21,6 @@ $sentencia = $pdo->prepare("INSERT INTO tb_usuarios
 $sentencia->bindParam(':nombres', $nombres);
 $sentencia->bindParam(':apellido', $apellido);
 $sentencia->bindParam(':email', $email);
-$sentencia->bindParam(':password_user', $password_user);
+$sentencia->bindParam(':password_user', $password_encriptada);
 $sentencia->bindParam(':fyh_creacion', $fechaHora);
 $sentencia->execute();
