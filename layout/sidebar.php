c2f28c6bfecc174bff3064b3d408d5af42906684
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-22 03:10:34 -0300

    coneccion a vercel

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,24 +1,16 @@
-  <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <a href="<?php echo $URL;  ?>/index.php" class="brand-link">
       <img src="<?php echo $URL; ?>public/images/pngwing.com.png" alt="Sistema de ventas Logo" class="brand-image img-circle" style="opacity: .8">
       <span class="brand-text font-weight-light">Sistema de ventas</span>
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="<?php echo $URL ?>public/templates/AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo ucfirst(strtolower($nombre_tabla))." ".ucfirst(strtolower($apellido_tabla)); ?></a>
         </div>
       </div>
 
-      <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
@@ -44,7 +36,6 @@
               </li>
             </ul>
           </li>
-          <!-- roles -->
           <li class="nav-item">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-address-book"></i>
@@ -125,6 +116,5 @@
 
         </ul>
       </nav>
-      <!-- /.sidebar-menu -->
     </div>
   </aside>
