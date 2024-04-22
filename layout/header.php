6c16837579c2709ac084136eed670dc64ea9ceb3
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-22 01:51:44 -0300

    agregando plugins de adminlte para usar estilos y iconos

--- layout/header.php
+++ layout/header.php
@@ -7,9 +7,18 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/all.min.css">
   <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/adminlte.min.css">
-  <!-- DataTables -->
   <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/dist/css/adminlte.min.css?v=3.2.0">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?php echo $URL;  ?>public/css/AdminLTE/plugins/summernote/summernote-bs4.min.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
