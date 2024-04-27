59bd374e213ccaf42c051fcd66c3fe35bd864c24
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-27 11:19:22 -0300

    eliminando includes

--- index.php
+++ index.php
@@ -5,7 +5,77 @@ INCLUDE ('./layout/header.php');
 INCLUDE ('./layout/nav.php');
 INCLUDE ('./layout/sidebar.php');
 INCLUDE ('./app/alerts.php');
+require_once(__DIR__ . '/vendor/autoload.php');
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('SERVIDOR', $_ENV['SERVIDOR']);
+define('USUARIO', $_ENV['USUARIO']);
+define('PASSWORD', $_ENV['PASSWORD']);
+define('BD', $_ENV['BD']);
+
+$servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
+
+try {
+    $pdo = new PDO(
+        $servidor, 
+        USUARIO, 
+        PASSWORD, 
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+    );
+} catch (PDOException $e) {
+    echo "Error al conectar a la base de datos: " . $e->getMessage();
+}
+
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+$fechaHora = date('Y-m-d H:i:s');
+
+$URL = "https://sistemadeventasapp-b723da2b8729.herokuapp.com/";
+
+session_start();
+
+if(isset($_SESSION['sesion_email'])) {
+    $email_sesion = $_SESSION['sesion_email'];
+
+    // Verificar si la variable de sesión está definida
+    if(!empty($email_sesion)) {
+        // Consulta preparada para seguridad contra inyección SQL
+        $sql = "SELECT * FROM tb_usuarios WHERE email = :email";
+        $query = $pdo->prepare($sql);
+        $query->bindParam(':email', $email_sesion);
+        $query->execute();
+
+        // Manejo de errores de base de datos
+        if($query) {
+            $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            if($usuarios){
+                foreach ($usuarios as $usuario) {
+                    $nombre_tabla = $usuario['nombres'];
+                    $apellido_tabla = $usuario['apellido'];
+                }
+            } else {
+                $nombre_tabla = "Sin Nombre";
+            }
+        } else {
+            // Manejo de errores de base de datos
+            echo "Error en la consulta SQL";
+        }
+    } else {
+        // Manejo de sesión no definida
+        echo "La variable de sesión está vacía";
+        header('Location:' . $URL . 'src/login.php');
+        exit(); // Terminar la ejecución del script después de redirigir
+    }
+} else {
+    // Manejo de sesión no existente
+    echo "No existe sesión";
+    header('Location:' . $URL . 'src/login.php');
+    exit(); // Terminar la ejecución del script después de redirigir
+}
 
 ?>
   <link rel="stylesheet" href="./public/css/index.css">
